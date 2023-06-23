06a9dfac81018b5ac3f5993cdb80f7955bb0dce2
dreadster3 <afonso.antunes@live.com.pt>
2023-06-23 19:29:38 +0100

    Fix issues with latex directories

--- nvim/lua/dreadster/set.lua
+++ nvim/lua/dreadster/set.lua
@@ -1,3 +1,4 @@
+-- General settings
 vim.opt.autoindent = true
 vim.opt.number = true
 vim.opt.ignorecase = true
@@ -8,18 +9,30 @@ vim.opt.smartindent = true
 vim.opt.incsearch = true
 vim.opt.wrap = false
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
 
+-- NvimTree Settings
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Fold settings
 vim.opt.foldlevel = 20
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
+-- LSP settings
 vim.g.completeopt = {"menuone", "noselect", "noinsert" }
 
+-- Copilot settings
 vim.g.copilot_assume_mapped = true
 
+-- Vimtex settings
+vim.g.vimtex_quickfix_open_on_warning = 0
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_compiler_latexmk = {
+	build_dir = "build",
+	aux_dir = "build",
+	out_dir = "build"
+}
