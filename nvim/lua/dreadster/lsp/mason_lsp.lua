5c4f543f0701ebb6b3a24db5ac5f2946d2bc3c92
dreadster3 <afonso.antunes@live.com.pt>
2023-06-30 22:17:55 +0100

    Fix clangd warning

--- nvim/lua/dreadster/lsp/mason_lsp.lua
+++ nvim/lua/dreadster/lsp/mason_lsp.lua
@@ -4,10 +4,10 @@ if not utils.check_module_installed("mason-lspconfig") then return end
 local mason_lsp = require("mason-lspconfig")
 
 mason_lsp.setup({
-    ensure_installed = {
-        "lua_ls", "clangd", "cmake", "tsserver", "terraformls", "tflint",
-        "pyright", "bashls"
-    }
+	ensure_installed = {
+		"lua_ls", "clangd", "cmake", "tsserver", "terraformls", "tflint",
+		"pyright", "bashls"
+	}
 })
 
 local lspconfig = require("lspconfig")
@@ -16,41 +16,44 @@ local capabilities = handlers.capabilities
 local on_attach = handlers.on_attach
 
 mason_lsp.setup_handlers({
-    function(server_name) -- default handler (optional)
-        lspconfig[server_name].setup({
-            capabilities = capabilities,
-            on_attach = on_attach
-        })
-    end,
-
-    ["lua_ls"] = function()
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = {
-                Lua = {
-                    diagnostics = {globals = {'vim'}},
-                    workspace = {checkThirdParty = false}
-                }
-            }
-        })
-    end,
-
-    ["pyright"] = function()
-        lspconfig.pyright.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            single_file_support = true
-        })
-    end,
-
-    ["rust_analyzer"] = function() require('dreadster.lsp.rust_tools') end,
-
-    ["grammarly"] = function()
-        lspconfig.grammarly.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            filetypes = {"markdown", "text", "tex"}
-        })
-    end
+	function(server_name) -- default handler (optional)
+		lspconfig[server_name].setup({
+			capabilities = capabilities,
+			on_attach = on_attach
+		})
+	end,
+	["lua_ls"]        = function()
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					diagnostics = { globals = { 'vim' } },
+					workspace = { checkThirdParty = false }
+				}
+			}
+		})
+	end,
+	["pyright"]       = function()
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			single_file_support = true
+		})
+	end,
+	["rust_analyzer"] = function() require('dreadster.lsp.rust_tools') end,
+	["grammarly"]     = function()
+		lspconfig.grammarly.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "markdown", "text", "tex" }
+		})
+	end,
+	["clangd"]        = function()
+		capabilities.offsetEncoding = "utf-8"
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+	end
 })
