92234a45ab513b0dc1b81637504763a30d4aef65
dreadster3 <afonso.antunes@live.com.pt>
2023-07-19 19:29:14 +0100

    Alter fidget spinner

--- nvim/lua/dreadster/fidget.lua
+++ nvim/lua/dreadster/fidget.lua
@@ -1,4 +1,4 @@
 local utils = require("dreadster.utils")
 if not utils.check_module_installed("fidget") then return end
 
-require("fidget").setup({})
+require("fidget").setup({ text = { spinner = "arc" } })
