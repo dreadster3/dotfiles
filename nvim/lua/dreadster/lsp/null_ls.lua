96a14e4d4e80ca923757c62e64a54827df34a5cc
dreadster3 <afonso.antunes@live.com.pt>
2023-08-03 21:20:18 +0100

    Fix condition to add to nullls

--- nvim/lua/dreadster/lsp/null_ls.lua
+++ nvim/lua/dreadster/lsp/null_ls.lua
@@ -2,12 +2,12 @@ local utils = require("dreadster.utils")
 if not utils.check_module_installed("null-ls") then return end
 
 local function addIfExecutable(list, command, value)
-    if not vim.fn.executable(command) == 1 then
-        vim.notify(command .. " not found.", vim.log.levels.WARN,
-                   {title = "null-ls"})
-        return
-    end
-    table.insert(list, value)
+	if vim.fn.executable(command) ~= 1 then
+		vim.notify(command .. " not found.", vim.log.levels.DEBUG,
+			{ title = "null-ls" })
+		return
+	end
+	table.insert(list, value)
 end
 
 local null_ls = require('null-ls')
@@ -18,12 +18,12 @@ local diagnostics = null_ls.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local lsp_formatting = function(bufnr)
-    if vim.g.skip_formatting then return end
+	if vim.g.skip_formatting then return end
 
-    vim.lsp.buf.format({timeout_ms = 2000, bufnr = bufnr})
+	vim.lsp.buf.format({ timeout_ms = 2000, bufnr = bufnr })
 end
 
-local sources = {formatting.clang_format}
+local sources = { formatting.clang_format }
 
 addIfExecutable(sources, "prettier", formatting.prettier)
 addIfExecutable(sources, "csharpier", formatting.csharpier)
@@ -36,18 +36,18 @@ addIfExecutable(sources, "clang-format", formatting.clang_format)
 addIfExecutable(sources, "tfsec", diagnostics.tfsec)
 
 null_ls.setup({
-    sources = sources,
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    lsp_formatting(bufnr)
-                end
-            })
-        end
-    end
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					lsp_formatting(bufnr)
+				end
+			})
+		end
+	end
 })
