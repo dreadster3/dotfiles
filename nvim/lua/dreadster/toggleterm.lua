56a28815a96942a929264d1ae35fdfcba1424b10
dreadster3 <afonso.antunes@live.com.pt>
2023-08-15 00:02:56 +0100

    Make toggleterm not persistent

--- nvim/lua/dreadster/toggleterm.lua
+++ nvim/lua/dreadster/toggleterm.lua
@@ -1,4 +1,4 @@
 local utils = require("dreadster.utils")
 if not utils.check_module_installed("toggleterm") then return end
 
-require('toggleterm').setup({open_mapping = [[<c-\>]]})
+require('toggleterm').setup({open_mapping = [[<c-\>]], persist_size = false})
