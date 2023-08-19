56ada2a12eed0ea77d0b05ee4ffe65070a530d91
dreadster3 <afonso.antunes@live.com.pt>
2023-08-19 01:44:30 +0100

    Readd commented code

--- nvim/lua/dreadster_lazy/plugins/lsp.lua
+++ nvim/lua/dreadster_lazy/plugins/lsp.lua
@@ -75,79 +75,79 @@ return {
             }
         },
         config = function(_, opts)
-            -- -- Diagnostics
-            -- for _, sign in ipairs(opts.diagnostics.signs.active) do
-            --     vim.fn.sign_define(sign.name, {
-            --         texthl = sign.name,
-            --         text = sign.text,
-            --         numhl = ""
-            --     })
-            -- end
-            --
-            -- vim.diagnostic.config(opts.diagnostics)
-            --
-            -- local handlers = require("dreadster_lazy.utils.lsp_handlers")
-            --
-            -- handlers.on_attach()
-            --
-            -- -- Servers
-            -- local servers = opts.servers
-            -- local has_cmp_nvim_lsp, cmp_nvim_lsp =
-            --     pcall(require, "cmp_nvim_lsp")
-            --
-            -- local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp
-            --                                              .protocol
-            --                                              .make_client_capabilities(),
-            --                                          has_cmp_nvim_lsp and
-            --                                              cmp_nvim_lsp.default_capabilities() or
-            --                                              {},
-            --                                          opts.capabilities or {})
-            --
-            -- local function setup(server)
-            --     local server_opts = vim.tbl_deep_extend("force", {
-            --         capabilities = vim.deepcopy(capabilities or {})
-            --     }, servers[server] or {})
-            --
-            --     if opts.setup[server] then
-            --         if opts.setup[server](server, server_opts) then
-            --             return
-            --         end
-            --     elseif opts.setup["*"] then
-            --         if opts.setup["*"](server, server_opts) then
-            --             return
-            --         end
-            --     end
-            --     require("lspconfig")[server].setup(server_opts)
-            -- end
-            --
-            -- -- get all the servers that are available thourgh mason-lspconfig
-            -- local have_mason, mlsp = pcall(require, "mason-lspconfig")
-            -- local all_mslp_servers = {}
-            -- if have_mason then
-            --     all_mslp_servers = vim.tbl_keys(require(
-            --                                         "mason-lspconfig.mappings.server").lspconfig_to_package)
-            -- end
-            --
-            -- local ensure_installed = {} ---@type string[]
-            -- for server, server_opts in pairs(servers) do
-            --     if server_opts then
-            --         server_opts = server_opts == true and {} or server_opts
-            --         -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-            --         if server_opts.mason == false or
-            --             not vim.tbl_contains(all_mslp_servers, server) then
-            --             setup(server)
-            --         else
-            --             ensure_installed[#ensure_installed + 1] = server
-            --         end
-            --     end
-            -- end
-            --
-            -- if have_mason then
-            --     mlsp.setup({
-            --         ensure_installed = ensure_installed,
-            --         handlers = {setup}
-            --     })
-            -- end
+            -- Diagnostics
+            for _, sign in ipairs(opts.diagnostics.signs.active) do
+                vim.fn.sign_define(sign.name, {
+                    texthl = sign.name,
+                    text = sign.text,
+                    numhl = ""
+                })
+            end
+
+            vim.diagnostic.config(opts.diagnostics)
+
+            local handlers = require("dreadster_lazy.utils.lsp_handlers")
+
+            handlers.on_attach()
+
+            -- Servers
+            local servers = opts.servers
+            local has_cmp_nvim_lsp, cmp_nvim_lsp =
+                pcall(require, "cmp_nvim_lsp")
+
+            local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp
+                                                         .protocol
+                                                         .make_client_capabilities(),
+                                                     has_cmp_nvim_lsp and
+                                                         cmp_nvim_lsp.default_capabilities() or
+                                                         {},
+                                                     opts.capabilities or {})
+
+            local function setup(server)
+                local server_opts = vim.tbl_deep_extend("force", {
+                    capabilities = vim.deepcopy(capabilities or {})
+                }, servers[server] or {})
+
+                if opts.setup[server] then
+                    if opts.setup[server](server, server_opts) then
+                        return
+                    end
+                elseif opts.setup["*"] then
+                    if opts.setup["*"](server, server_opts) then
+                        return
+                    end
+                end
+                require("lspconfig")[server].setup(server_opts)
+            end
+
+            -- get all the servers that are available thourgh mason-lspconfig
+            local have_mason, mlsp = pcall(require, "mason-lspconfig")
+            local all_mslp_servers = {}
+            if have_mason then
+                all_mslp_servers = vim.tbl_keys(require(
+                                                    "mason-lspconfig.mappings.server").lspconfig_to_package)
+            end
+
+            local ensure_installed = {} ---@type string[]
+            for server, server_opts in pairs(servers) do
+                if server_opts then
+                    server_opts = server_opts == true and {} or server_opts
+                    -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+                    if server_opts.mason == false or
+                        not vim.tbl_contains(all_mslp_servers, server) then
+                        setup(server)
+                    else
+                        ensure_installed[#ensure_installed + 1] = server
+                    end
+                end
+            end
+
+            if have_mason then
+                mlsp.setup({
+                    ensure_installed = ensure_installed,
+                    handlers = {setup}
+                })
+            end
         end
     }, {"williamboman/mason.nvim", name = "mason", opts = {}}, {
         "glepnir/lspsaga.nvim",
