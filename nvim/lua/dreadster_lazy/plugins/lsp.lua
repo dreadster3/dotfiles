a7b5105f003e5d5df39300ecfd0c2cc4cffb5bdb
dreadster3 <afonso.antunes@live.com.pt>
2023-08-21 03:01:05 +0100

    Fix autocmds

--- nvim/lua/dreadster_lazy/plugins/lsp.lua
+++ nvim/lua/dreadster_lazy/plugins/lsp.lua
@@ -172,6 +172,35 @@ return {
 }, { "ray-x/go.nvim",        event = { "BufReadPost *.go" }, opts = {} }, {
 	"Saecki/crates.nvim",
 	event = { "BufRead Cargo.toml" },
+	init = function()
+		vim.api.nvim_create_autocmd("BufReadPre", {
+			group = vim.api.nvim_create_augroup("CmpSourceCargo",
+				{ clear = true }),
+			pattern = "Cargo.toml",
+			callback = function()
+				local cmp = require("cmp")
+				cmp.setup.buffer({ sources = { { name = "crates" } } })
+
+				local opts = { noremap = true, silent = true }
+				local keymap = vim.api.nvim_buf_set_keymap
+				keymap(0, 'n', '<F4>',
+					"<cmd>lua require('crates').show_popup()<CR>", opts)
+				keymap(0, 'n', 'cf',
+					"<cmd>lua require('crates').show_features_popup()<CR>",
+					opts)
+				keymap(0, 'n', 'cr',
+					"<cmd>lua require('crates').reload()<CR>", opts)
+				keymap(0, 'n', 'cd',
+					"<cmd>lua require('crates').open_crates_io()<CR>",
+					opts)
+				keymap(0, 'n', 'cu',
+					"<cmd>lua require('crates').update_crate()<CR>", opts)
+				keymap(0, 'n', 'cU',
+					"<cmd>lua require('crates').update_all_crates()<CR>",
+					opts)
+			end
+		})
+	end,
 	config = function(_, opts)
 		require("crates").setup(opts)
 
--- nvim/lua/dreadster_lazy/plugins/tools.lua
+++ nvim/lua/dreadster_lazy/plugins/tools.lua
@@ -1,209 +1,262 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		name = "telescope",
-		dependencies = {
-			"nvim-lua/plenary.nvim", {
-			"nvim-telescope/telescope-media-files.nvim",
-			dependencies = { "nvim-lua/popup.nvim" }
-		}, "nvim-telescope/telescope-symbols.nvim", "project", "template"
-		},
-		event = { "User AlphaReady" },
-		cmd = { "Telescope" },
-		keys = {
-			{ "<leader>ff", ":Telescope find_files<CR>", desc = "Find files" },
-			{ "<C-f>",      ":Telescope live_grep<CR>",  desc = "Live grep" },
-			{
-				"<leader>fm",
-				":Telescope media_files<CR>",
-				desc = "Find media files"
-			}, { "<leader>fp", ":Telescope projects<CR>", desc = "Find projects" }
-		},
-		config = function(_, opts)
-			local telescope = require("telescope")
-			telescope.setup(opts)
+    {
+        "nvim-telescope/telescope.nvim",
+        name = "telescope",
+        dependencies = {
+            "nvim-lua/plenary.nvim", {
+                "nvim-telescope/telescope-media-files.nvim",
+                dependencies = {"nvim-lua/popup.nvim"}
+            }, "nvim-telescope/telescope-symbols.nvim", "project", "template"
+        },
+        event = {"User AlphaReady"},
+        cmd = {"Telescope"},
+        keys = {
+            {"<leader>ff", ":Telescope find_files<CR>", desc = "Find files"},
+            {"<C-f>", ":Telescope live_grep<CR>", desc = "Live grep"},
+            {
+                "<leader>fm",
+                ":Telescope media_files<CR>",
+                desc = "Find media files"
+            }, {"<leader>fp", ":Telescope projects<CR>", desc = "Find projects"}
+        },
+        config = function(_, opts)
+            local telescope = require("telescope")
+            telescope.setup(opts)
 
-			telescope.load_extension("media_files")
-			telescope.load_extension("find_template")
-			telescope.load_extension("projects")
-		end,
-		opts = function()
-			local opts = {
-				defaults = {
-					file_ignore_patterns = {
-						"obj", "bin", "node_modules", "build", "target"
-					},
-					layout_strategy = "horizontal",
-					layout_config = {
-						horizontal = {
-							prompt_position = "top",
-							preview_width = 0.55,
-							results_width = 0.8
-						},
-						vertical = { mirror = false },
-						width = 0.87,
-						height = 0.80,
-						preview_cutoff = 120
-					},
-					borderchars = {
-						"─", "│", "─", "│", "╭", "╮", "╯", "╰"
-					},
-					mappings = {
-						n = { ["q"] = require("telescope.actions").close }
-					}
-				},
-				extensions = {}
-			}
+            telescope.load_extension("media_files")
+            telescope.load_extension("find_template")
+            telescope.load_extension("projects")
+        end,
+        opts = function()
+            local opts = {
+                defaults = {
+                    file_ignore_patterns = {
+                        "obj", "bin", "node_modules", "build", "target"
+                    },
+                    layout_strategy = "horizontal",
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "top",
+                            preview_width = 0.55,
+                            results_width = 0.8
+                        },
+                        vertical = {mirror = false},
+                        width = 0.87,
+                        height = 0.80,
+                        preview_cutoff = 120
+                    },
+                    borderchars = {
+                        "─", "│", "─", "│", "╭", "╮", "╯", "╰"
+                    },
+                    mappings = {
+                        n = {["q"] = require("telescope.actions").close}
+                    }
+                },
+                extensions = {}
+            }
 
-			return opts
-		end
-	}, {
-	"kdheepak/lazygit.nvim",
-	name = "lazygit",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	cmd = "LazyGit",
-	keys = { { "<leader>g", "<CMD>LazyGit<CR>", desc = "Lanch lazygit" } }
-}, {
-	"akinsho/toggleterm.nvim",
-	name = "toggleterm",
-	init = function()
-		local opts = { noremap = true, buffer = 0 }
+            return opts
+        end
+    }, {
+        "kdheepak/lazygit.nvim",
+        name = "lazygit",
+        dependencies = {"nvim-lua/plenary.nvim"},
+        cmd = "LazyGit",
+        keys = {{"<leader>g", "<CMD>LazyGit<CR>", desc = "Lanch lazygit"}}
+    }, {
+        "akinsho/toggleterm.nvim",
+        dependencies = {"lualine"},
+        name = "toggleterm",
+        version = "*",
+        init = function()
+            -- Terminal Toggle
+            function _G.set_terminal_keymaps()
+                local opts = {noremap = true, buffer = 0}
 
-		if vim.bo.filetype ~= 'toggleterm' then return end
+                if vim.bo.filetype ~= 'toggleterm' then return end
 
-		-- vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-		vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-		vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-		vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-		vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-		vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-		vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-	end,
-	cmd = "ToggleTerm",
-	keys = { { "<C-\\>", "<CMD>ToggleTerm<CR>", desc = "Toggle terminal" } },
-	opts = { open_mapping = [[<c-\>]], persist_size = false }
-}, {
-	"iamcco/markdown-preview.nvim",
-	name = "markdown-preview",
-	version = "*",
-	cmd = { "MarkdownPreviewToggle" },
-	build = "cd app && npm install",
-	ft = "markdown",
-	keys = {
-		{
-			"<leader>md",
-			"<CMD>MarkdownPreviewToggle<CR>",
-			desc = "Toggle markdown preview"
-		}
-	}
-},
-	{ "hiphish/rainbow-delimiters.nvim", name = "rainbow", event = "BufReadPre" },
-	{
-		"glepnir/template.nvim",
-		name = "template",
-		cmd = "Template",
-		init = function()
-			local mappings = {
-				typescript = { "function.ts" },
-				cmake = { "VCMakeLists.txt" },
-				text = { "conanfile.txt" },
-				yaml = { "clangformat" }
-			}
+                vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+                vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+                vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+                vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+                vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+                vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+            end
 
-			local temp_table = {}
+            vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
+        end,
+        cmd = "ToggleTerm",
+        keys = {{"<C-\\>", "<CMD>ToggleTerm<CR>", desc = "Toggle terminal"}},
+        opts = {
+            open_mapping = [[<c-\>]],
+            persist_size = false,
+            start_in_insert = true,
+            persist_mode = false
+        }
+    }, {
+        "iamcco/markdown-preview.nvim",
+        name = "markdown-preview",
+        version = "*",
+        cmd = {"MarkdownPreviewToggle"},
+        build = "cd app && npm install",
+        ft = "markdown",
+        keys = {
+            {
+                "<leader>md",
+                "<CMD>MarkdownPreviewToggle<CR>",
+                desc = "Toggle markdown preview"
+            }
+        }
+    },
+    {"hiphish/rainbow-delimiters.nvim", name = "rainbow", event = "BufReadPre"},
+    {
+        "glepnir/template.nvim",
+        name = "template",
+        cmd = "Template",
+        init = function()
+            local mappings = {
+                typescript = {"function.ts"},
+                cmake = {"VCMakeLists.txt"},
+                text = {"conanfile.txt"},
+                yaml = {"clangformat"}
+            }
 
-			for filetype, filenames in pairs(mappings) do
-				for _, filename in ipairs(filenames) do
-					temp_table[filename] = filetype
-				end
-			end
+            local temp_table = {}
 
-			-- Add any template that filetype cannot be detected
-			vim.filetype.add({ filename = temp_table })
-		end,
-		opts = {
-			author = "dreadster3",
-			email = "afonso.antunes@live.com.pt",
-			temp_dir = "/home/dreadster/.config/nvim/template"
-		}
-	}, {
-	"johnfrankmorgan/whitespace.nvim",
-	name = "whitespace",
-	event = { "BufReadPre", "BufNewFile" },
-	cmd = { "WhitespaceTrim" },
-	init = function()
-		vim.api.nvim_create_user_command("WhitespaceTrim", function()
-			require("whitespace").trim()
-		end, {})
-	end,
-	opts = {
-		highlight = 'DiffDelete',
-		ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help', "noice" },
-		ignore_terminal = true
-	}
-}, {
-	"ahmedkhalf/project.nvim",
-	name = "project",
-	cmd = "ProjectRoot",
-	config = function(_, opts) require("project_nvim").setup(opts) end,
-	opts = {
-		active = true,
-		on_config_done = nil,
-		manual_mode = true,
-		detection_methods = { "pattern" },
-		patterns = {
-			".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile",
-			"package.json"
-		},
-		show_hidden = false,
-		silent_chdir = false,
-		ignore_lsp = {},
-		datapath = vim.fn.stdpath("data")
-	}
-}, {
-	"dreadster3/neovim-tasks",
-	name = "tasks",
-	cmd = "Task",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = function(_, opts)
-		local path = require("plenary.path")
-		return {
-			default_params = {
-				cmake = {
-					cmd = "cmake",
-					build_dir = tostring(
-						path:new('{cwd}', 'build', '{os}-{build_type}')),
-					build_type = "Debug",
-					args = { configure = { "-G", "Unix Makefiles" } }
-				},
-				conan = { cmd = "conan", build_type = "Debug" }
-			},
-			quickfix = { only_on_error = true }
-		}
-	end
-}, { "danymat/neogen",               name = "neogen",  cmd = "Neogen",      opts = {} }, {
-	"cshuaimin/ssr.nvim",
-	name = "ssr",
-	keys = {
-		{
-			"<leader>sr",
-			"<CMD>lua require('ssr').open()<CR>",
-			desc = "Search and Replace"
-		}
-	},
-	opts = {
-		border = "rounded",
-		min_width = 50,
-		min_height = 5,
-		max_width = 120,
-		max_height = 25,
-		keymaps = {
-			close = "q",
-			next_match = "n",
-			prev_match = "N",
-			replace_confirm = "<cr>",
-			replace_all = "<leader><cr>"
-		}
-	}
-}
+            for filetype, filenames in pairs(mappings) do
+                for _, filename in ipairs(filenames) do
+                    temp_table[filename] = filetype
+                end
+            end
+
+            -- Add any template that filetype cannot be detected
+            vim.filetype.add({filename = temp_table})
+        end,
+        opts = {
+            author = "dreadster3",
+            email = "afonso.antunes@live.com.pt",
+            temp_dir = "/home/dreadster/.config/nvim/template"
+        }
+    }, {
+        "johnfrankmorgan/whitespace.nvim",
+        name = "whitespace",
+        event = {"BufReadPre", "BufNewFile"},
+        cmd = {"WhitespaceTrim"},
+        init = function()
+            vim.api.nvim_create_user_command("WhitespaceTrim", function()
+                require("whitespace").trim()
+            end, {})
+        end,
+        opts = {
+            highlight = 'DiffDelete',
+            ignored_filetypes = {'TelescopePrompt', 'Trouble', 'help', "noice"},
+            ignore_terminal = true
+        }
+    }, {
+        "ahmedkhalf/project.nvim",
+        name = "project",
+        cmd = "ProjectRoot",
+        config = function(_, opts) require("project_nvim").setup(opts) end,
+        opts = {
+            active = true,
+            on_config_done = nil,
+            manual_mode = true,
+            detection_methods = {"pattern"},
+            patterns = {
+                ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile",
+                "package.json"
+            },
+            show_hidden = false,
+            silent_chdir = false,
+            ignore_lsp = {},
+            datapath = vim.fn.stdpath("data")
+        }
+    }, {
+        "dreadster3/neovim-tasks",
+        name = "tasks",
+        cmd = "Task",
+        dependencies = {"nvim-lua/plenary.nvim"},
+        init = function()
+            local augroup = vim.api.nvim_create_augroup
+            local au = vim.api.nvim_create_autocmd
+            local opts = {noremap = true, buffer = 0}
+            local nnoremap = function(lhs, rhs)
+                vim.keymap.set('n', lhs, rhs, opts)
+            end
+
+            local cpp_maps = {
+                pattern = {
+                    "CMakeLists.txt", "conanfile.txt", "*.cpp", "*.hpp", "*.h"
+                },
+                keymaps = {
+                    {"<leader>cb", ":Task start cmake build<CR>"},
+                    {"<leader>cg", ":Task start cmake configure<CR>"},
+                    {"<leader>ci", ":Task start conan install<CR>"}
+                }
+            }
+
+            local terraform_maps = {
+                pattern = {"*.tf"},
+                keymaps = {
+                    {"<leader>tfi", ":Task start terraform init<CR>"},
+                    {"<leader>tfv", ":Task start terraform validate<CR>"}
+                }
+            }
+
+            local maps = {cpp_maps, terraform_maps}
+
+            local tasks_group = augroup("tasks", {clear = true})
+            for _, map in ipairs(maps) do
+                au("BufEnter", {
+                    group = tasks_group,
+                    pattern = map.pattern,
+                    callback = function()
+                        for _, keymap in ipairs(map.keymaps) do
+                            nnoremap(keymap[1], keymap[2])
+                        end
+                    end
+                })
+            end
+        end,
+        opts = function(_, opts)
+            local path = require("plenary.path")
+            return {
+                default_params = {
+                    cmake = {
+                        cmd = "cmake",
+                        build_dir = tostring(
+                            path:new('{cwd}', 'build', '{os}-{build_type}')),
+                        build_type = "Debug",
+                        args = {configure = {"-G", "Unix Makefiles"}}
+                    },
+                    conan = {cmd = "conan", build_type = "Debug"}
+                },
+                quickfix = {only_on_error = true}
+            }
+        end
+    }, {"danymat/neogen", name = "neogen", cmd = "Neogen", opts = {}}, {
+        "cshuaimin/ssr.nvim",
+        name = "ssr",
+        keys = {
+            {
+                "<leader>sr",
+                "<CMD>lua require('ssr').open()<CR>",
+                desc = "Search and Replace"
+            }
+        },
+        opts = {
+            border = "rounded",
+            min_width = 50,
+            min_height = 5,
+            max_width = 120,
+            max_height = 25,
+            keymaps = {
+                close = "q",
+                next_match = "n",
+                prev_match = "N",
+                replace_confirm = "<cr>",
+                replace_all = "<leader><cr>"
+            }
+        }
+    }
 }
