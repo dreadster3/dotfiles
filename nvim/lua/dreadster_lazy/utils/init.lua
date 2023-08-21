2f031ca0c8c5a012f7370517f7097ea44e133d0a
Nuno Antunes <nuno.antunes@saint-gobain.com>
2023-08-21 10:20:38 +0100

    Improve lazy load of neodev

--- nvim/lua/dreadster_lazy/utils/init.lua
+++ nvim/lua/dreadster_lazy/utils/init.lua
@@ -3,44 +3,44 @@ local M = {}
 --- Check if a module is installed and log a warning if it is not
 ---@param module_name name of the module to check
 M.log_module_failed_load = function(module_name)
-	vim.notify("Failed to load module", vim.log.levels.WARN,
-		{ title = module_name })
+    vim.notify("Failed to load module", vim.log.levels.WARN,
+               {title = module_name})
 end
 
 --- Check if a module is installed
----@param module_name name of the module to check
+---@param  module_name string of the module to check
 ---@return boolean true if the module is installed, false otherwise
 M.check_module_installed = function(module_name)
-	local ok, _ = pcall(require, module_name)
+    local ok, _ = pcall(require, module_name)
 
-	if not ok then
-		M.log_module_failed_load(module_name)
-		return false
-	end
+    if not ok then
+        M.log_module_failed_load(module_name)
+        return false
+    end
 
-	return true
+    return true
 end
 
 M.is_mac = function() return vim.fn.has("macunix") == 1 end
 
 M.is_linux = function()
-	local uname = vim.loop.os_uname()
+    local uname = vim.loop.os_uname()
 
-	return uname.sysname == "Linux"
+    return uname.sysname == "Linux"
 end
 
 M.is_wsl = function()
-	local uname = vim.loop.os_uname()
+    local uname = vim.loop.os_uname()
 
-	local is_microsoft = uname.release:find("microsoft") ~= nil
+    local is_microsoft = uname.release:find("microsoft") ~= nil
 
-	return M.is_linux() and is_microsoft
+    return M.is_linux() and is_microsoft
 end
 
 M.is_in_table = function(table, element)
-	for _, value in ipairs(table) do if value == element then return true end end
+    for _, value in ipairs(table) do if value == element then return true end end
 
-	return false
+    return false
 end
 
 return M
