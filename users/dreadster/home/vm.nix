16a79718576c1a75a465b4e169319fdedff3ce39
dreadster3 <afonso.antunes@live.com.pt>
2023-10-22 18:00:28 +0100

    Add x11 event call backs as service

--- users/dreadster/home/vm.nix
+++ users/dreadster/home/vm.nix
@@ -1,10 +1,39 @@
 {config, lib, pkgs, ...}:
+let
+	x11package = pkgs.callPackage ../../../derivations/x11eventcallbacks.nix {};
+in
 {
 	imports = [
 		./default.nix
 	];
 
 	home.packages = with pkgs; [
-		(pkgs.callPackage ../../../derivations/x11eventcallbacks.nix {})
+		(x11package)
 	];
+
+	systemd.user.services.x11eventcallbacks = {
+		Unit = {
+			Description = "X11 event callbacks";
+			After = [ "graphical.target" ];
+		};
+
+		Install = {
+			WantedBy = [ "default.target" ];
+		};
+
+		Service = {
+			ExecStart = "${x11package}/bin/x11_event_callbacks " + pkgs.writers.writeBash "restart_polybar.sh" ''
+				nitrogen --restore 2> /dev/null
+				$HOME/.config/polybar/shades/launch.sh & disown
+			'';
+			Environment = [
+				"DISPLAY=:0"
+				"PATH=/run/current-system/sw/bin:${pkgs.killall}/bin:${pkgs.polybar}/bin"
+			];
+			StandardOutput = "journal+console";
+			StandardError = "journal+console";
+			Restart = "always";
+			RestartSec = "5";
+		};
+	};
 }
