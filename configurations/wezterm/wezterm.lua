b609b6d9bd7ef7c5328e6695105cfb2408d7346c
Nuno Antunes <nuno.antunes@saint-gobain.com>
2023-11-24 15:39:55 +0000

    Add keybinds for wezterm

--- configurations/wezterm/wezterm.lua
+++ configurations/wezterm/wezterm.lua
@@ -5,49 +5,49 @@ local config = {}
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 18.0
 config.font_rules = {
-    {
-        intensity = 'Bold',
-        italic = true,
-        font = wezterm.font {
-            family = 'VictorMono Nerd Font',
-            weight = 'Bold',
-            style = 'Italic'
-        }
-    }, {
-        italic = true,
-        intensity = 'Half',
-        font = wezterm.font {
-            family = 'VictorMono Nerd Font',
-            weight = 'DemiBold',
-            style = 'Italic'
-        }
-    }, {
-        italic = true,
-        intensity = 'Normal',
-        font = wezterm.font {family = 'VictorMono Nerd Font', style = 'Italic'}
-    }
+	{
+		intensity = 'Bold',
+		italic = true,
+		font = wezterm.font {
+			family = 'VictorMono Nerd Font',
+			weight = 'Bold',
+			style = 'Italic'
+		}
+	}, {
+	italic = true,
+	intensity = 'Half',
+	font = wezterm.font {
+		family = 'VictorMono Nerd Font',
+		weight = 'DemiBold',
+		style = 'Italic'
+	}
+}, {
+	italic = true,
+	intensity = 'Normal',
+	font = wezterm.font { family = 'VictorMono Nerd Font', style = 'Italic' }
+}
 }
 
 config.color_scheme = 'Catppuccin Mocha'
 config.tab_bar_at_bottom = true
 config.background = {
-    {
-        source = {
-            File = os.getenv("HOME") ..
-                "/Documents/projects/github/dotfiles/configurations/wallpapers/cat_bunnies.png"
-        },
-        opacity = 1.0,
-        hsb = {brightness = 0.0, saturation = 1.0, hue = 1.0},
-        attachment = "Fixed"
-    }, {
-        source = {
-            File = os.getenv("HOME") ..
-                "/Documents/projects/github/dotfiles/configurations/wallpapers/cat_bunnies.png"
-        },
-        opacity = 0.2,
-        hsb = {brightness = 1.8, saturation = 1.0, hue = 1.0},
-        attachment = "Fixed"
-    }
+	{
+		source = {
+			File = os.getenv("HOME") ..
+				"/Documents/projects/github/dotfiles/configurations/wallpapers/cat_bunnies.png"
+		},
+		opacity = 1.0,
+		hsb = { brightness = 0.0, saturation = 1.0, hue = 1.0 },
+		attachment = "Fixed"
+	}, {
+	source = {
+		File = os.getenv("HOME") ..
+			"/Documents/projects/github/dotfiles/configurations/wallpapers/cat_bunnies.png"
+	},
+	opacity = 0.2,
+	hsb = { brightness = 1.8, saturation = 1.0, hue = 1.0 },
+	attachment = "Fixed"
+}
 }
 
 config.enable_tab_bar = true
@@ -75,18 +75,18 @@ config.keys = {
 }
 
 if utils.is_mac() then
-    local macos = require("macos")
-    config = utils.merge({config, macos})
+	local macos = require("macos")
+	config = utils.merge({ config, macos })
 end
 
 if utils.is_linux() then
-    local windows = require("linux")
-    config = utils.merge({config, windows})
+	local windows = require("linux")
+	config = utils.merge({ config, windows })
 end
 
 if utils.is_windows() then
-    local windows = require("windows")
-    config = utils.merge({config, windows})
+	local windows = require("windows")
+	config = utils.merge({ config, windows })
 end
 
 return config
