d926805a9ba882fc798e0e0216efbcefb02c488e
dreadster3 <afonso.antunes@live.com.pt>
2024-03-06 11:25:08 +0000

    Add git messenger

--- configurations/nvim/lua/dreadster/plugins/helpers.lua
+++ configurations/nvim/lua/dreadster/plugins/helpers.lua
@@ -1,46 +1,46 @@
 return {
-    {
-        "terrortylor/nvim-comment",
-        name = "nvim_comment",
-        dependencies = {"JoosepAlviste/nvim-ts-context-commentstring"},
-        keys = {
-            {"<C-/>", ":CommentToggle<CR>", desc = "Toggle comment for line"}, {
-                "<C-/>",
-                ":'<,'>CommentToggle<CR>",
-                mode = "v",
-                desc = "Toggle comment for line"
-            }
-        },
-        opts = {
-            hook = function()
-                require('ts_context_commentstring.internal').update_commentstring(
-                    {})
-            end
-        }
-    }, {
-        'windwp/nvim-autopairs',
-        name = "autopairs",
-        dependencies = {
-            {'windwp/nvim-ts-autotag', name = "autopairs-ts", opts = {}}
-        },
-        event = {"InsertEnter"},
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {check_ts = true}
-    }, {"folke/which-key.nvim", event = "VeryLazy", opts = {}},
-    {"folke/todo-comments.nvim", event = "BufReadPre", opts = {}},
-    {"RRethy/vim-illuminate", event = {"BufReadPost"}}, {
-        "gbprod/yanky.nvim",
-        lazy = false,
-        keys = {
-            {"y", "<Plug>(YankyYank)", mode = {"n", "x"}},
-            {"p", "<Plug>(YankyPutAfter)", mode = {"n", "x"}},
-            {"P", "<Plug>(YankyPutBefore)", mode = {"n", "x"}},
-            {"<c-n>", "<Plug>(YankyCycleForward)", mode = {"n", "x"}},
-            {"<c-p>", "<Plug>(YankyCycleBackward)", mode = {"n", "x"}}
-        },
-        opts = {}
-    }
+	{
+		"terrortylor/nvim-comment",
+		name = "nvim_comment",
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+		keys = {
+			{ "<C-/>", ":CommentToggle<CR>", desc = "Toggle comment for line" }, {
+			"<C-/>",
+			":'<,'>CommentToggle<CR>",
+			mode = "v",
+			desc = "Toggle comment for line"
+		}
+		},
+		opts = {
+			hook = function()
+				require('ts_context_commentstring.internal').update_commentstring(
+					{})
+			end
+		}
+	}, {
+	'windwp/nvim-autopairs',
+	name = "autopairs",
+	dependencies = {
+		{ 'windwp/nvim-ts-autotag', name = "autopairs-ts", opts = {} }
+	},
+	event = { "InsertEnter" },
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = { check_ts = true }
+}, { "folke/which-key.nvim",  event = "VeryLazy",     opts = {} },
+	{ "folke/todo-comments.nvim", event = "BufReadPre",   opts = {} },
+	{ "RRethy/vim-illuminate",    event = { "BufReadPost" } }, {
+	"gbprod/yanky.nvim",
+	lazy = false,
+	keys = {
+		{ "y",     "<Plug>(YankyYank)",          mode = { "n", "x" } },
+		{ "p",     "<Plug>(YankyPutAfter)",      mode = { "n", "x" } },
+		{ "P",     "<Plug>(YankyPutBefore)",     mode = { "n", "x" } },
+		{ "<c-n>", "<Plug>(YankyCycleForward)",  mode = { "n", "x" } },
+		{ "<c-p>", "<Plug>(YankyCycleBackward)", mode = { "n", "x" } }
+	},
+	opts = {}
+}, { "rhysd/git-messenger.vim", cmd = { "GitMessenger" }, opts = {} }
 }
