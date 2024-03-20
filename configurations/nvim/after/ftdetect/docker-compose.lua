fb79263ce9c0fb0d2fa1b55406a2cd12fa1e5b6b
dreadster3 <afonso.antunes@live.com.pt>
2024-03-20 14:42:07 +0000

    Autodetect docker compose filetypes

---
+++ configurations/nvim/after/ftdetect/docker-compose.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = {
+        "docker-compose.yaml", "docker-compose.yml", "compose.yaml",
+        "compose.yml"
+    },
+    callback = function() vim.bo.filetype = "yaml.docker-compose" end
+})
