9fd80f65c823499bd886478e6e0a0d5f82292b4b
dreadster3 <afonso.antunes@live.com.pt>
2024-05-04 05:16:12 +0100

    Add thunar extras

--- nix/modules/nixos/thunar.nix
+++ nix/modules/nixos/thunar.nix
@@ -5,7 +5,17 @@ in {
   options = { modules.nixos.thunar = { enable = mkEnableOption "thunar"; }; };
 
   config = mkIf cfg.enable {
-    programs = { thunar.enable = true; };
-    services = { tumbler.enable = true; };
+    environment.systemPackages = with pkgs; [ xfce.exo ];
+
+    programs.xfconf.enable = true;
+
+    programs.thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
+    };
+    services = {
+      gvfs.enable = true;
+      tumbler.enable = true;
+    };
   };
 }
