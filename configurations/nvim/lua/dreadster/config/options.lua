de93a8dffe6ccdf3750600d0592a654164556b8a
dreadster3 <afonso.antunes@live.com.pt>
2024-05-27 02:20:34 +0100

    conf(nvim): add ufo plugin

--- configurations/nvim/lua/dreadster/config/options.lua
+++ configurations/nvim/lua/dreadster/config/options.lua
@@ -25,31 +25,34 @@ vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = " "
 
 -- Fold settings
-vim.opt.foldlevel = 20
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = true
+vim.opt.foldcolumn = '1' -- '0' is not bad
+vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 vim.opt.background = "dark"
 
 -- Copy + Paste WSL
 local utils = require("dreadster.utils")
 if utils.is_wsl() then
-	vim.g.clipboard = {
-		name = "WslClipboard",
-		copy = { ["+"] = "clip.exe", ["*"] = "clip.exe" },
-		paste = {
-			['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-			['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
-		},
-		cache_enabled = 0
-	}
+    vim.g.clipboard = {
+        name = "WslClipboard",
+        copy = {["+"] = "clip.exe", ["*"] = "clip.exe"},
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
+        },
+        cache_enabled = 0
+    }
 end
 
 -- Vimtex settings
 vim.g.vimtex_quickfix_open_on_warning = 0
 vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_compiler_latexmk = {
-	build_dir = "build",
-	aux_dir = "build",
-	out_dir = "build"
+    build_dir = "build",
+    aux_dir = "build",
+    out_dir = "build"
 }
