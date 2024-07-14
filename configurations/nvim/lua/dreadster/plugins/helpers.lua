cc266e0184b47bd7a945bcbfa80aacd87722dd37
dreadster3 <afonso.antunes@live.com.pt>
2024-07-14 21:59:16 +0100

    feat(whichkey): increase delay

--- configurations/nvim/lua/dreadster/plugins/helpers.lua
+++ configurations/nvim/lua/dreadster/plugins/helpers.lua
@@ -39,17 +39,26 @@ return {
 		end,
 		opts = { check_ts = true },
 	},
-	{ "folke/which-key.nvim",     verison = "*",            event = "VeryLazy", opts = {} },
-	{ "folke/todo-comments.nvim", event = "BufReadPre",     opts = {} },
-	{ "RRethy/vim-illuminate",    event = { "BufReadPost" } },
+	{
+		"folke/which-key.nvim",
+		version = "*",
+		event = "VeryLazy",
+		opts = {
+			delay = function(ctx)
+				return 500
+			end,
+		},
+	},
+	{ "folke/todo-comments.nvim", event = "BufReadPre", opts = {} },
+	{ "RRethy/vim-illuminate", event = { "BufReadPost" } },
 	{
 		"gbprod/yanky.nvim",
 		lazy = false,
 		keys = {
-			{ "y",     "<Plug>(YankyYank)",          mode = { "n", "x" } },
-			{ "p",     "<Plug>(YankyPutAfter)",      mode = { "n", "x" } },
-			{ "P",     "<Plug>(YankyPutBefore)",     mode = { "n", "x" } },
-			{ "<c-n>", "<Plug>(YankyCycleForward)",  mode = { "n", "x" } },
+			{ "y", "<Plug>(YankyYank)", mode = { "n", "x" } },
+			{ "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" } },
+			{ "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" } },
+			{ "<c-n>", "<Plug>(YankyCycleForward)", mode = { "n", "x" } },
 			{ "<c-p>", "<Plug>(YankyCycleBackward)", mode = { "n", "x" } },
 		},
 		opts = {},
