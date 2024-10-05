cf44c06efc13fe7253f4c2d87732dff7ebf5c3c7
dreadster3 <afonso.antunes@live.com.pt>
2024-10-05 15:30:19 +0100

    feat(vmware): allow macos guests

--- nix/modules/nixos/vmware.nix
+++ nix/modules/nixos/vmware.nix
@@ -2,13 +2,25 @@
 with lib;
 let cfg = config.modules.nixos.vmware;
 in {
-  options = { modules.nixos.vmware = { enable = mkEnableOption "vmware"; }; };
+  options = {
+    modules.nixos.vmware = {
+      enable = mkEnableOption "vmware";
+      package = mkOption {
+        type = types.package;
+        default = pkgs.vmware-workstation;
+        description = "The VMware Tools package to install.";
+      };
+    };
+  };
 
   config = mkIf cfg.enable {
-    virtualisation.vmware.host.enable = true;
-    virtualisation.vmware.host.extraConfig = ''
-      wsFeatureDarkModeSupported = "TRUE"
-      mks.gl.allowUnsupportedDrivers = "TRUE"
-      mks.vk.allowUnsupportedDevices = "TRUE"'';
+    virtualisation.vmware.host = {
+      enable = true;
+      package = cfg.package.override { enableMacOSGuests = true; };
+      extraConfig = ''
+        wsFeatureDarkModeSupported = "TRUE"
+        mks.gl.allowUnsupportedDrivers = "TRUE"
+        mks.vk.allowUnsupportedDevices = "TRUE"'';
+    };
   };
 }
