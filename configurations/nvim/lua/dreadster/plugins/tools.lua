420cde33ac918bc6f14db6a964759852c8421885
dreadster3 <afonso.antunes@live.com.pt>
2024-11-11 00:19:32 +0000

    feat(nvim): markdown renderer

--- configurations/nvim/lua/dreadster/plugins/tools.lua
+++ configurations/nvim/lua/dreadster/plugins/tools.lua
@@ -276,4 +276,13 @@ return {
 			require("neotest").setup(opts)
 		end,
 	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		name = "render-markdown",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+		ft = { "markdown" },
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
 }
