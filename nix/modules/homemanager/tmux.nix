f6fb3dc919a8d56c2f2df08ba77eada4392bcfff
dreadster3 <afonso.antunes@live.com.pt>
2024-11-24 02:25:42 +0000

    improve tmux ui

--- nix/modules/homemanager/tmux.nix
+++ nix/modules/homemanager/tmux.nix
@@ -1,8 +1,7 @@
 { pkgs, config, lib, ... }:
 with lib;
 let cfg = config.modules.homemanager.tmux;
-in
-{
+in {
   options = { modules.homemanager.tmux = { enable = mkEnableOption "tmux"; }; };
 
   config = mkIf cfg.enable {
@@ -16,13 +15,15 @@ in
       mouse = true;
       terminal = "tmux-256color";
       plugins = with pkgs.tmuxPlugins; [
-        better-mouse-mode
         {
           plugin = catppuccin;
           extraConfig = ''
             set -g @catppuccin_flavour 'mocha'
             set -g @catppuccin_window_tabs_enabled 'on'
             set -g @catppuccin_date_time "%H:%M"
+            set -g @catppuccin_window_left_separator ""
+            set -g @catppuccin_window_right_separator " "
+            set -g @catppuccin_status_modules_right "application cpu session host date_time"
           '';
         }
         {
@@ -38,16 +39,15 @@ in
           extraConfig = ''
             set -g @continuum-restore 'on'
             set -g @continuum-boot 'on'
-            set -g @continuum-save-interval '30'
+            set -g @continuum-save-interval '10'
           '';
         }
+        cpu
+        better-mouse-mode
       ];
       extraConfig = ''
         bind-key v split-window -v
         bind-key h split-window -h
-
-        set-option status on
-        set -g status-right 'Continuum status: #{continuum_status}'
       '';
     };
   };
