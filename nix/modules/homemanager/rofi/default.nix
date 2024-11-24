608bdf9f264a948c996a16653ce991cc3fabb5ac
dreadster3 <afonso.antunes@live.com.pt>
2024-11-24 23:32:38 +0000

    feat(rofi): make use of rofi for wayland

--- nix/modules/homemanager/rofi/default.nix
+++ nix/modules/homemanager/rofi/default.nix
@@ -1,16 +1,18 @@
 { config, lib, pkgs, username, ... }:
 with lib;
 let
+  inherit (config.lib.formats.rasi) mkLiteral;
   cfg = config.modules.homemanager.rofi;
   packagePath = getExe config.programs.rofi.finalPackage;
   terminal = either cfg.terminal config.modules.homemanager.settings.terminal;
+  accent = config.catppuccin.accent;
 in {
   options = {
     modules.homemanager.rofi = {
       enable = mkEnableOption "rofi";
       package = mkOption {
         type = types.package;
-        default = pkgs.rofi;
+        default = pkgs.rofi-wayland;
         description = "Package to use for rofi";
       };
       font = mkOption {
@@ -48,6 +50,11 @@ in {
         "${packagePath} -show p -modi 'p:${getExe cfg.powermenu.package}'";
     };
 
+    wayland.windowManager.hyprland.settings.bind = [
+      "$mainMod, D, exec, pkill rofi || ${packagePath} -show drun"
+      "$mainMod, Space, exec, pkill rofi || ${packagePath} -show drun"
+    ];
+
     programs.rofi = {
       enable = true;
       package = cfg.package;
@@ -55,11 +62,15 @@ in {
       plugins = [ ] ++ optional cfg.powermenu.enable cfg.powermenu.package;
       terminal = getExe terminal;
       location = "center";
-      theme = import ./theme.nix { inherit config lib; };
+      theme = {
+        window.border = mkLiteral "3px solid";
+        window.border-radius = mkLiteral "12px";
+        window.border-color = mkLiteral "@${accent}";
+      };
       extraConfig = {
         modi = "drun";
         show-icons = true;
-        display-drun = "";
+        display-drun = " ";
         drun-display-format = "{name}";
       };
     };
