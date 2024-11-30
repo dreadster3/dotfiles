b5e84db1119504b9703067d6830f66cc7009f298
dreadster3 <afonso.antunes@live.com.pt>
2024-11-30 05:54:31 +0000

    feat fail2ban

---
+++ nix/modules/nixos/fail2ban.nix
@@ -0,0 +1,21 @@
+{ pkgs, config, lib, ... }:
+with lib;
+let cfg = config.modules.nixos.fail2ban;
+in {
+  options = {
+    modules.nixos.fail2ban = { enable = mkEnableOption "fail2ban"; };
+  };
+
+  config = mkIf cfg.enable {
+    services.fail2ban = {
+      enable = true;
+      maxretry = 3;
+      bantime = "1h";
+      bantime-increment = {
+        enable = true;
+        factor = "24";
+        overalljails = true;
+      };
+    };
+  };
+}
