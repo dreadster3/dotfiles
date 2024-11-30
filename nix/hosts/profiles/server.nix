d9e1771208dbaf43f71ecdf619d8ded5b20d186a
dreadster3 <afonso.antunes@live.com.pt>
2024-11-30 11:59:17 +0000

    feat: add desktop pub key

--- nix/hosts/profiles/server.nix
+++ nix/hosts/profiles/server.nix
@@ -6,8 +6,9 @@ in {
   modules.nixos = { fail2ban.enable = true; };
 
   users.users.dreadster.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP29pHHpOPwY4wgvyuhx4n2Qu/md6KL78uvXBA+5cSAt dreadster@WSL"
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCE4y4iP00784oPIFjy8Ad+eWIRW1QPW74jXA/jQVltU8AI8XRKGCUduK8TLopbqZplKfZVgiKqOuMvvYhkQzvS4D1qawPrdamQ+bvTnI4ehZAU7K3lzKTWm2AABytiv9Xm1hF4/wqD/iPxz18rj+mHeoEjcAsNDT/7mPR3EAS0yOg784jfgmKVLyZZGZIcTgaW+SrVkJ68KKA2xlWNTHbALw0NuR1liMDPnEmOXJ0caplBZPH2rbABnR5A7q35FyJbt/3kzjT8T7LXUX51X40/4E20JS8fFvbEZq6EfFmgs8+HsGm3ZH/u6umNt9w3StHCc3c/i0IUodd2tpRjEeD root@pve30"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP29pHHpOPwY4wgvyuhx4n2Qu/md6KL78uvXBA+5cSAt dreadster@WSL"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGCMowytry00kaNo1EMXPhZb0WiEANL9AD1oj9V0f0Ad dreadster@nixos-desktop"
   ];
   security.sudo.wheelNeedsPassword = false;
 }
