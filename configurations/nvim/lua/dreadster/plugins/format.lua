c31a01333041ddd211080b9fcb5ee4b625d9d7d3
dreadster3 <afonso.antunes@live.com.pt>
2025-01-28 01:25:37 +0000

    feat(nvim): add golang linting

--- configurations/nvim/lua/dreadster/plugins/format.lua
+++ configurations/nvim/lua/dreadster/plugins/format.lua
@@ -39,8 +39,56 @@ return {
 				lua = { "luacheck" },
 				python = { "ruff", "mypy" },
 				rust = { "clippy" },
-				terraform = { "trivy", "tflint" },
+				terraform = { "tflint", "trivy" },
+				go = { "staticcheck", "trivy", "nilaway" },
 				["*"] = { "typos" },
+			},
+			linters = {
+				staticcheck = {
+					cmd = "staticcheck",
+					args = { "-f", "json", "./..." },
+					append_fname = false,
+				},
+				nilaway = {
+					cmd = "nilaway",
+					args = { "-json", "-pretty-print=false", "./..." },
+					append_fname = false,
+					stdin = false,
+					stream = "stdout",
+					ignore_exitcode = true,
+					parser = function(output, bufnr)
+						local diagnostics = {}
+						local decoded = vim.json.decode(output)
+						local bufname = vim.api.nvim_buf_get_name(bufnr)
+
+						for _, nilaway in pairs(decoded) do
+							local messages = nilaway.nilaway
+							for _, result in ipairs(messages) do
+								local position_str = result.posn
+								local splits = vim.split(position_str, ":")
+								local filename = splits[1]
+								local row = tonumber(splits[2]) - 1
+								local column = tonumber(splits[3]) - 1
+								local message = result.message
+
+								if filename ~= bufname then
+									break
+								end
+
+								table.insert(diagnostics, {
+									lnum = row,
+									col = column,
+									end_col = column,
+									end_lnum = row,
+									severity = vim.diagnostic.severity.WARN,
+									message = message,
+								})
+							end
+						end
+
+						return diagnostics
+					end,
+				},
 			},
 		},
 		config = function(_, opts)
@@ -61,13 +109,25 @@ return {
 				end
 			end
 
+			for name, linter in pairs(opts.linters) do
+				if type(linter) == "table" and type(lint.linters[name]) == "table" then
+					lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
+				else
+					lint.linters[name] = linter
+				end
+			end
+
 			lint.linters_by_ft = opts.linters_by_ft
 			vim.api.nvim_create_autocmd(opts.events, {
 				group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
 				callback = debounce(100, function()
-					require("lint").try_lint()
+					lint.try_lint()
 				end),
 			})
+
+			vim.api.nvim_create_user_command("LintInfo", function()
+				print(vim.inspect(lint.get_running()))
+			end, {})
 		end,
 	},
 }
