557c0269313cf5cc430d9669471f586cc414927d
dreadster3 <afonso.antunes@live.com.pt>
2025-03-23 21:04:38 +0000

    test

--- nix/hosts/vm/configuration.nix
+++ nix/hosts/vm/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ hostname, config, pkgs, ... }: {
+{ hostname, config, pkgs, lib, ... }: {
   imports = [
     ../profiles/personal.nix
 
