7ef0d0472e3c21e5c927671a19010c994366caa7
dreadster3 <afonso.antunes@live.com.pt>
2025-03-23 00:29:30 +0000

    test

--- nix/home/dreadster/vm.nix
+++ nix/home/dreadster/vm.nix
@@ -19,32 +19,20 @@ in {
 
     gtk.enable = true;
     dunst.enable = true;
-    firefox.enable = true;
 
     # X11
     polybar = {
       enable = true;
       networkInterface = { name = "ens33"; };
     };
-    rofi.enable = true;
-    rofi.package = pkgs.rofi;
-    # sxhkd.enable = true;
-    # bspwm = {
-    #   enable = true;
-    #   startupPrograms = [
-    #     "${pkgs.open-vm-tools}/bin/vmware-user-suid-wrapper"
-    #     "${
-    #       lib.getExe pkgs.xorg.xrandr
-    #     } --output ${primaryMonitor} --primary --pos 1080x240 --output ${secondaryMonitor} --pos 0x0"
-    #   ];
-    # };
-    # flameshot.enable = true;
-    # mechvibes.enable = true;
-    # x11eventcallbacks.enable = true;
+    rofi = {
+      enable = true;
+      package = pkgs.rofi;
+    };
+    flameshot.enable = true;
+    x11eventcallbacks.enable = true;
     # betterlockscreen.enable = true;
-    # guake.enable = true;
 
-    # Other
     pentest.enable = true;
   };
 }
