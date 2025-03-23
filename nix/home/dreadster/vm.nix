5dfb2d8443fab6ac418bdf6d3c5117bdcf41a0ce
dreadster3 <afonso.antunes@live.com.pt>
2025-03-23 00:10:45 +0000

    test

--- nix/home/dreadster/vm.nix
+++ nix/home/dreadster/vm.nix
@@ -1,7 +1,5 @@
 { inputs, outputs, config, lib, pkgs, pkgs-unstable, ... }:
-let
-  primaryMonitor = "Virtual-1";
-  secondaryMonitor = "Virtual-2";
+let primaryMonitor = "Virtual1";
 in {
   imports = [ ./base/personal.nix ];
 
@@ -15,7 +13,6 @@ in {
             primary = true;
             workspaces = [ 1 2 3 4 5 ];
           };
-          "${secondaryMonitor}" = { workspaces = [ 6 7 8 9 10 ]; };
         };
       };
     };
@@ -29,22 +26,22 @@ in {
       enable = true;
       networkInterface = { name = "ens33"; };
     };
-    rofi.enable = true;
-    sxhkd.enable = true;
-    bspwm = {
-      enable = true;
-      startupPrograms = [
-        "${pkgs.open-vm-tools}/bin/vmware-user-suid-wrapper"
-        "${
-          lib.getExe pkgs.xorg.xrandr
-        } --output ${primaryMonitor} --primary --pos 1080x240 --output ${secondaryMonitor} --pos 0x0"
-      ];
-    };
-    flameshot.enable = true;
-    mechvibes.enable = true;
-    x11eventcallbacks.enable = true;
-    betterlockscreen.enable = true;
-    guake.enable = true;
+    # rofi.enable = true;
+    # sxhkd.enable = true;
+    # bspwm = {
+    #   enable = true;
+    #   startupPrograms = [
+    #     "${pkgs.open-vm-tools}/bin/vmware-user-suid-wrapper"
+    #     "${
+    #       lib.getExe pkgs.xorg.xrandr
+    #     } --output ${primaryMonitor} --primary --pos 1080x240 --output ${secondaryMonitor} --pos 0x0"
+    #   ];
+    # };
+    # flameshot.enable = true;
+    # mechvibes.enable = true;
+    # x11eventcallbacks.enable = true;
+    # betterlockscreen.enable = true;
+    # guake.enable = true;
 
     # Other
     pentest.enable = true;
