4b26a23fd63dd152976a4c9fe9f151c0499b683e
dreadster3 <afonso.antunes@live.com.pt>
2025-03-23 20:20:52 +0000

    test

--- nix/modules/nixos/bspwm.nix
+++ nix/modules/nixos/bspwm.nix
@@ -1,8 +1,7 @@
 { config, pkgs, lib, ... }:
 with lib;
 let cfg = config.modules.nixos.bspwm;
-in
-{
+in {
   options = { modules.nixos.bspwm = { enable = mkEnableOption "bspwm"; }; };
 
   config = mkIf cfg.enable {
@@ -11,13 +10,13 @@ in
       message = "xserver must be enabled to use bspwm";
     }];
 
-    services.xserver.windowManager.bspwm.enable = true; };
-
-    services.xserver.desktopManager.xterm.enable = false;
-
-    services.displayManager.defaultSession = mkDefault "none+bspwm";
-
-    home-manager.sharedModules =
-      [{ modules.homemanager.bspwm.enable = mkDefault true; }];
+    services.xserver.windowManager.bspwm.enable = true;
   };
+
+  services.xserver.desktopManager.xterm.enable = false;
+
+  services.displayManager.defaultSession = mkDefault "none+bspwm";
+
+  home-manager.sharedModules =
+    [{ modules.homemanager.bspwm.enable = mkDefault true; }];
 }
