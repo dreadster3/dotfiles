0b47e144ab301f94fece622268c70dd571bedf7d
dreadster3 <afonso.antunes@live.com.pt>
2025-03-23 17:23:32 +0000

    remove i3 module

--- nix/modules/homemanager/polybar.nix
+++ nix/modules/homemanager/polybar.nix
@@ -414,23 +414,6 @@ in {
             label-empty-padding = 1;
           };
 
-          "module/i3" = {
-            type = "internal/i3";
-            pin-workspaces = true;
-            index-sort = true;
-            enable-scroll = true;
-            wrapping_scroll = true;
-
-            ws-icon-0 = "1;";
-            ws-icon-1 = "2;";
-            ws-icon-2 = "3;";
-            ws-icon-3 = "4;";
-
-            label-separator = "|";
-            label-separator-padding = 2;
-            label-separator-foreground = "#ffb52a";
-          };
-
           "module/filesystem" = {
             type = "internal/fs";
 
