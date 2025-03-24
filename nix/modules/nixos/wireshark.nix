61d7c50bb9e8a3172766852b935562e8b5ebd576
dreadster3 <afonso.antunes@live.com.pt>
2025-03-24 01:39:41 +0000

    feat: add wireshark gui

--- nix/modules/nixos/wireshark.nix
+++ nix/modules/nixos/wireshark.nix
@@ -3,8 +3,20 @@ with lib;
 let cfg = config.modules.nixos.wireshark;
 in {
   options = {
-    modules.nixos.wireshark = { enable = mkEnableOption "wireshark"; };
+    modules.nixos.wireshark = {
+      enable = mkEnableOption "wireshark";
+
+      package = mkOption {
+        type = types.package;
+        default = pkgs.wireshark;
+      };
+    };
   };
 
-  config = mkIf cfg.enable { programs.wireshark = { enable = true; }; };
+  config = mkIf cfg.enable {
+    programs.wireshark = {
+      enable = true;
+      package = cfg.package;
+    };
+  };
 }
