f67380f19a5963042d5335b77fa77be39d54fe6f
dreadster3 <afonso.antunes@live.com.pt>
2025-03-30 22:05:40 +0100

    fix: dap issues

--- configurations/nvim/lua/dreadster/plugins/codeium.lua
+++ configurations/nvim/lua/dreadster/plugins/codeium.lua
@@ -2,6 +2,8 @@ return {
 	{
 		"Exafunction/codeium.nvim",
 		main = "codeium",
+		cmd = { "Codeium" },
+		build = ":Codeium Auth",
 		enabled = function()
 			return not require("dreadster.utils").is_mac()
 		end,
