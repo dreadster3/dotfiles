ddc98ca789a71154747009a9633400f4766d6c48
dreadster3 <afonso.antunes@live.com.pt>
2025-04-05 04:14:39 +0100

    feat: hyprlock background

--- nix/modules/homemanager/hyprlock.nix
+++ nix/modules/homemanager/hyprlock.nix
@@ -32,33 +32,8 @@ in {
       enable = true;
       package = cfg.package;
       settings = {
-        general = {
-          disable_loading_bar = false;
-          grace = 0;
-          no_fade_in = true;
-          no_fade_out = true;
-          hide_cursor = true;
-        };
-
-        background = [{
-          path = toString ../../../wallpapers/city_mountains.jpg;
-          blur_passes = 1;
-          blur_size = 5;
-        }];
-
-        input-field = [{
-          size = "200, 50";
-          position = "0, -80";
-          monitor = "";
-          dots_center = true;
-          fade_on_empty = false;
-          font_color = "rgb(202, 211, 245)";
-          inner_color = "rgb(91, 96, 120)";
-          outer_color = "rgb(24, 25, 38)";
-          outline_thickness = 5;
-          placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
-          shadow_passes = 2;
-        }];
+        background =
+          [{ path = toString ../../../wallpapers/ctp_line_mocha_mauve.jpg; }];
       };
     };
 
