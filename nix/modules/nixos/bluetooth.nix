5da3ad584128ff94c9e55bb42ed40b62c24d511c
dreadster3 <afonso.antunes@live.com.pt>
2025-11-23 19:47:34 +0000

    feat(nix): dualshock udev rules

--- nix/modules/nixos/bluetooth.nix
+++ nix/modules/nixos/bluetooth.nix
@@ -10,8 +10,14 @@ in {
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
+      settings = { General = { Experimental = true; }; };
     };
 
+    services.udev.extraRules = ''
+      # Sony PlayStation DualShock 4; Bluetooth; USB
+      ACTION!="remove", KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0660", TAG+="uaccess"
+      ACTION!="remove", KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0660", TAG+="uaccess"
+    '';
     services.blueman.enable = true;
   };
 }
