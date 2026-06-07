2270bf10190037b38833866c06a7060dc0f352b3
dreadster3 <afonso.antunes@live.com.pt>
2026-06-07 23:22:34 +0100

    fix: friendly-snippets setup

--- configurations/nvim/lua/dreadster/plugins/lang/snippets.lua
+++ configurations/nvim/lua/dreadster/plugins/lang/snippets.lua
@@ -4,18 +4,16 @@ return {
 		version = "*",
 		name = "luasnip",
 		dependencies = {
-			{
-				"rafamadriz/friendly-snippets",
-				config = function()
-					require("luasnip.loaders.from_vscode").lazy_load()
-					require("luasnip.loaders.from_vscode").lazy_load({
-						paths = { vim.fn.stdpath("config") .. "/snippets" },
-					})
-				end,
-			},
+			"rafamadriz/friendly-snippets",
 		},
 		lazy = true,
 		build = "make install_jsregexp",
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load({
+				paths = { vim.fn.stdpath("config") .. "/snippets" },
+			})
+		end,
 		opts = {
 			history = true,
 			delete_check_events = "TextChanged",
