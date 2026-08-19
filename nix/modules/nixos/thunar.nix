97d2d603d9af8b30b9cb9db2ed91133acf2d0274
dreadster3 <afonso.antunes@live.com.pt>
2026-08-20 00:32:13 +0100

    chore: update nix

--- nix/modules/nixos/thunar.nix
+++ nix/modules/nixos/thunar.nix
@@ -16,7 +16,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs.stable; [ exo ];
+    environment.systemPackages = with pkgs; [ xfce4-exo ];
 
     programs.xfconf.enable = true;
 
