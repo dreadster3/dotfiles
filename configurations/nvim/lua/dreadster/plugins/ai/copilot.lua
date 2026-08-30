175a7c6ca5b670ec784e1df77b44d598e931b3df
dreadster3 <afonso.antunes@live.com.pt>
2026-08-30 21:51:30 +0100

    feat(nvim): enable supermaven

--- configurations/nvim/lua/dreadster/plugins/ai/copilot.lua
+++ configurations/nvim/lua/dreadster/plugins/ai/copilot.lua
@@ -1,103 +1,60 @@
+local is_copilot_enabled = function()
+    local copilot_env = vim.fn.getenv("NVIM_COPILOT_ENABLED")
+
+    if copilot_env == vim.NIL then
+        return require("dreadster.utils").is_mac()
+    end
+
+    copilot_env = copilot_env:gsub("^%s+", ""):gsub("%s+$", "")
+
+    local values = {
+        ["true"] = true,
+        ["yes"] = true,
+        ["1"] = true,
+        ["on"] = true,
+    }
+
+    return values[copilot_env:lower()]
+end
 return {
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		name = "copilot-chat",
-		cmd = { "CopilotChat", "CopilotChatAgents", "CopilotChatModels", "CopilotChatExplain" },
-		version = "*",
-		enabled = function()
-			-- TODO: remove if plan to keep copilot
-			-- local utils = require("dreadster.utils")
-			-- return utils.is_mac()
-			return true
-		end,
-		dependencies = {
-			{ "copilot" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-		},
-		build = "make tiktoken", -- Only on MacOS or Linux
-        -- stylua: ignore
-		keys = {
-			{ "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
-			{ "<leader>aa", function() return require("CopilotChat").toggle() end, desc = "Toggle (CopilotChat)", mode = { "n", "v" } },
-			{ "<leader>ax", function() return require("CopilotChat").reset() end, desc = "Clear (CopilotChat)", mode = { "n", "v" }, },
-			{ "<leader>aq", function() local input = vim.fn.input("Quick Chat: ") if input ~= "" then require("CopilotChat").ask(input) end end, desc = "Quick Chat (CopilotChat)", mode = { "n", "v" }, },
-			-- Show prompts actions with telescope
-			{ "<leader>ap", function() require("CopilotChat").select_prompt() end, desc = "Prompt Actions (CopilotChat)", mode = { "n", "v" } },
-		},
-		config = function(_, opts)
-			local chat = require("CopilotChat")
-
-			vim.api.nvim_create_autocmd("BufEnter", {
-				pattern = "copilot-chat",
-				callback = function()
-					vim.opt_local.relativenumber = false
-					vim.opt_local.number = false
-				end,
-			})
-
-			chat.setup(opts)
-		end,
-		opts = function()
-			local user = vim.env.USER or "User"
-			return {
-				question_header = "  " .. user .. " ",
-				model = "claude-sonnet-4",
-				answer_header = "  Copilot ",
-				auto_insert_mode = true,
-				mappings = {
-					reset = {
-						normal = "<leader>ax",
-						insert = "<C-l>",
-					},
-				},
-				window = {
-					width = 0.2,
-				},
-			}
-		end,
-	},
-	{
-		"zbirenbaum/copilot.lua",
-		name = "copilot",
-		version = "*",
-		build = ":Copilot auth",
-		enabled = function()
-			-- local utils = require("dreadster.utils")
-			-- return utils.is_mac()
-			return true
-		end,
-		cmd = { "Copilot" },
-		event = { "BufReadPost" },
-		opts = {
-			panel = {
-				enabled = false,
-				keymap = { enable = false },
-			},
-			suggestion = {
-				enabled = false,
-			},
-			filetypes = {
-				yaml = true,
-				markdown = true,
-				help = true,
-			},
-		},
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		lazy = true,
-		opts = {},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		optional = true,
-		dependencies = { "zbirenbaum/copilot-cmp" },
-		opts = function(_, opts)
-			table.insert(opts.sources, 1, {
-				name = "copilot",
-				priority = 1100,
-				group_index = 1,
-			})
-		end,
-	},
+    {
+        "zbirenbaum/copilot.lua",
+        name = "copilot",
+        version = "*",
+        build = ":Copilot auth",
+        enabled = is_copilot_enabled,
+        cmd = { "Copilot" },
+        event = { "BufReadPost" },
+        opts = {
+            panel = {
+                enabled = false,
+                keymap = { enable = false },
+            },
+            suggestion = {
+                enabled = false,
+            },
+            filetypes = {
+                yaml = true,
+                markdown = true,
+                help = true,
+            },
+        },
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        lazy = true,
+        opts = {},
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        optional = true,
+        dependencies = { "zbirenbaum/copilot-cmp" },
+        opts = function(_, opts)
+            table.insert(opts.sources, 1, {
+                name = "copilot",
+                priority = 1100,
+                group_index = 1,
+            })
+        end,
+    },
 }
--- configurations/nvim/lua/dreadster/plugins/ai/supermaven.lua
+++ configurations/nvim/lua/dreadster/plugins/ai/supermaven.lua
@@ -1,27 +1,42 @@
+local is_supermaven_enabled = function()
+    local copilot_env = vim.fn.getenv("NVIM_SUPERMAVEN_ENABLED")
+
+    if copilot_env == vim.NIL then
+        return not require("dreadster.utils").is_mac()
+    end
+
+    copilot_env = copilot_env:gsub("^%s+", ""):gsub("%s+$", "")
+
+    local values = {
+        ["true"] = true,
+        ["yes"] = true,
+        ["1"] = true,
+        ["on"] = true,
+    }
+
+    return values[copilot_env:lower()]
+end
 return {
-	{
-		"supermaven-inc/supermaven-nvim",
-		commit = "07d20fce48a5629686aefb0a7cd4b25e33947d50",
-		name = "supermaven",
-		event = "InsertEnter",
-		enabled = function()
-			-- return not require("dreadster.utils").is_mac()
-			return false
-		end,
-		opts = {
-			disable_inline_completion = true,
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		optional = true,
-		dependencies = { "supermaven" },
-		opts = function(_, opts)
-			table.insert(opts.sources, 1, {
-				name = "supermaven",
-				priority = 1100,
-				group_index = 1,
-			})
-		end,
-	},
+    {
+        "supermaven-inc/supermaven-nvim",
+        commit = "07d20fce48a5629686aefb0a7cd4b25e33947d50",
+        name = "supermaven",
+        event = "InsertEnter",
+        enabled = is_supermaven_enabled,
+        opts = {
+            disable_inline_completion = true,
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        optional = true,
+        dependencies = { "supermaven" },
+        opts = function(_, opts)
+            table.insert(opts.sources, 1, {
+                name = "supermaven",
+                priority = 1100,
+                group_index = 1,
+            })
+        end,
+    },
 }
